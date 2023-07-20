d306bb174358baf3453683d7a49c80f0e4aa03fe
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-20 20:30:33 +0300

    entity add model

--- app/src/main/java/com/example/datewise/data/local/model/DayModel.kt
+++ app/src/main/java/com/example/datewise/data/local/model/DayModel.kt
@@ -1,4 +1,22 @@
 package com.example.datewise.data.local.model
 
-class DayModel {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+
+@Entity(tableName = "days_table")
+data class DayModel (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id:Int=0,
+    @ColumnInfo(name = "day_image")
+    var image:String,
+    @ColumnInfo(name = "day_name")
+    var name:String,
+    @ColumnInfo(name = "day_dayName")
+    var dayname:String,
+    @ColumnInfo(name = "day_date")
+    var daydate:Date
+        )
