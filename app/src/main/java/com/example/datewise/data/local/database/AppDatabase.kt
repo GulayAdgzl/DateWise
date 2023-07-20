e0b07bce1c8dbcb96f18c2e3e2c3dc6030fc130b
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-07-21 01:16:47 +0300

    create AppDatabase class RoomDatabase extends

---
+++ app/src/main/java/com/example/datewise/data/local/database/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.datewise.data.local.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.datewise.data.local.dao.DayDAO
+import com.example.datewise.data.local.model.DayModel
+
+@Database(entities = [DayModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase:RoomDatabase() {
+    abstract val dayDao:DayDAO
+    companion object {
+
+        @Volatile
+        private var INSTANCE:AppDatabase?=null
+
+        fun getAppDatabase(context: Context):AppDatabase?{
+            //singelton
+            synchronized(this){
+                var instance= INSTANCE
+                if(instance == null){
+                    instance= Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "day_database"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE=instance
+                }
+                return INSTANCE
+            }
+
+        }
+    }
+}
